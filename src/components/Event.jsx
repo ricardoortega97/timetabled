7429c9e6026adde6bdb68c04a897b376c862b7bf
Ricardo Ortega Pacheco <ricardo.ortega97@outlook.com>
2024-09-17 22:06:27 -0400

    Initial Commit

---
+++ src/components/Event.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Event = (props) => {
+  
+    return (
+        <td className="Event"> 
+            <h5> Test Event Name </h5>
+        </td>
+    )
+}
+
+export default Event
